060e842f973943d93635728a918294111cf1afe3
Sara Gajic <sara08gajic@gmail.com>
2026-02-07 20:08:34 +0100

    add temperature control functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,16 @@
 import { useState } from "react";
 export default function DashboardPage() {
 const [lightOn, setLightOn] = useState(false);
+const [temperature, setTemperature] = useState(22);
+
+
   return (
     <main>
   <h1>Dashboard</h1>
   <p>Pregled pametnog okruženja</p>
 
   <h2>Svetlo u dnevnoj sobi</h2>
+  
 
   <p>
     Status: {lightOn ? "Uključeno" : "Isključeno"}
@@ -16,6 +20,17 @@ const [lightOn, setLightOn] = useState(false);
   <button onClick={() => setLightOn(!lightOn)}>
     {lightOn ? "Isključi svetlo" : "Uključi svetlo"}
   </button>
+
+  <h3>Klima uređaj</h3>
+
+<p>Temperatura: {temperature}°C</p>
+<button onClick={() => setTemperature(temperature + 1)}>
+  Povećaj temperaturu 
+</button>
+
+<button onClick={() => setTemperature(temperature - 1)}>
+Smanji temperaturu
+</button>
 </main>
   );
 }

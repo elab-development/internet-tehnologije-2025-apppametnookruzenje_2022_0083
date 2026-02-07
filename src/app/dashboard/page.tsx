7aa77d6db161610aa3e22fba6bab73e9c696aa95
Sara Gajic <sara08gajic@gmail.com>
2026-02-07 20:36:03 +0100

    deviceCard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { useState } from "react";
+import DeviceCard from "@/components/DeviceCard";
+
+
 export default function DashboardPage() {
 const [lightOn, setLightOn] = useState(false);
 const [temperature, setTemperature] = useState(22);
@@ -11,51 +14,46 @@ const [doorLocked, setDoorLocked] = useState(true);
   <h1>Dashboard</h1>
   <p>Pregled pametnog okruženja</p>
 
-  <h2>Svetlo u dnevnoj sobi</h2>
-  
-
-  <p>
-    Status: {lightOn ? "Uključeno" : "Isključeno"}
-  </p>
-
+ <DeviceCard
+  title="Svetlo u dnevnoj sobi"
+  status={lightOn ? "Uključeno" : "Isključeno"}
+>
   <button onClick={() => setLightOn(!lightOn)}>
     {lightOn ? "Isključi svetlo" : "Uključi svetlo"}
   </button>
+</DeviceCard>
 
-  <h3>Klima uređaj</h3>
-
-<p>Temperatura: {temperature}°C</p>
-<p>Dozvoljeni opseg: 16°C – 30°C</p>
-
-<button
-  onClick={() => {
-    if (temperature < 30) {
-      setTemperature(temperature + 1);
-    }
-  }}
+  <DeviceCard
+  title="Klima uređaj"
+  status={`${temperature}°C`}
 >
-  Povećaj temperaturu
-</button>
+  <button
+    onClick={() => {
+      if (temperature < 30) setTemperature(temperature + 1);
+    }}
+  >
+    Povećaj temperaturu
+  </button>
 
-<button
-  onClick={() => {
-    if (temperature > 16) {
-      setTemperature(temperature - 1);
-    }
-  }}
+  <button
+    onClick={() => {
+      if (temperature > 16) setTemperature(temperature - 1);
+    }}
+  >
+    Smanji temperaturu
+  </button>
+
+  <p>Dozvoljeni opseg: 16°C – 30°C</p>
+</DeviceCard>
+
+<DeviceCard
+  title="Pametna brava"
+  status={doorLocked ? "Zaključano" : "Otključano"}
 >
-  Smanji temperaturu
-</button>
-
-<h3>Pametna brava</h3>
-
-<p>
-  Status: {doorLocked ? "Zaključano" : "Otključano"}
-</p>
-
-<button onClick={() => setDoorLocked(!doorLocked)}>
-  {doorLocked ? "Otključaj" : "Zaključaj"}
-</button>
+  <button onClick={() => setDoorLocked(!doorLocked)}>
+    {doorLocked ? "Otključaj" : "Zaključaj"}
+  </button>
+</DeviceCard>
 
 </main>
   );
